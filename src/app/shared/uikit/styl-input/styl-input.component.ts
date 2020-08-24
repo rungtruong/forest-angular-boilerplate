6b356f415b8747d3ec86fbf7a8cd9f9392ca09f0
forest.truong <forest.truong@styl.solutions>
2020-08-24 17:16:19 +0700

    custom tree select

--- src/app/shared/uikit/styl-input/styl-input.component.ts
+++ src/app/shared/uikit/styl-input/styl-input.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./styl-input.component.less']
 })
 export class StylInputComponent implements OnInit {
+  passwordVisible = false;
+  password?: string = "";
+
+
+  value: string[] = [];
+  nodes = [
+    {
+      title: 'parent 1',
+      key: '100',
+      children: [
+        {
+          title: 'parent 1-0',
+          key: '1001',
+          children: [
+            { title: 'leaf 1-0-0', key: '10010', isLeaf: true },
+            { title: 'leaf 1-0-1', key: '10011', isLeaf: true }
+          ]
+        },
+        {
+          title: 'parent 1-1',
+          key: '1002',
+          children: [{ title: 'leaf 1-1-0', key: '10020', isLeaf: true }]
+        }
+      ]
+    }
+  ];
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onChange($event: string[]): void {
+    console.log($event);
+  }
 }
